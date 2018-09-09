0011be6cf0b90a68fcdf64e2c4ba1b607dbcf34a
ronggenliu <ronggenliu@gmail.com>
2018-09-09 11:24:25 +0800

    add main fun.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by ronggenliu on 9/9/18.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
